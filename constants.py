845d0caeb6e17d02126eb57f9319011d34bf37fa
Dariaivvi02 <Dariyaivanova06@gmail.com>
2025-09-26 14:53:48 +0500

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,4 @@
-# Настройки окна
-WIDTH = 800
-HEIGHT = 600
-FPS = 60
+"""Константы игры"""
 
 # Размеры объектов
 BASKET_W = 100
@@ -9,17 +6,9 @@ BASKET_H = 20
 BALL_R = 15
 ROCK_R = 20
 
-# Скорости объектов
-BASKET_SPEED = 7
-BALL_SPEED = 2
-ROCK_SPEED = 4
-
-# Частота появления объектов
-SPAWN_TIMER = 60
-
 # Цвета
-WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 RED = (220, 50, 50)
 BLUE = (50, 50, 220)
 GREEN = (50, 220, 50)
@@ -27,9 +16,15 @@ GRAY = (100, 100, 100)
 DARK_GRAY = (50, 50, 50)
 YELLOW = (255, 255, 0)
 
-# Цветовые наборы
-BALL_COLORS = [RED, BLUE, GREEN]
-ROCK_COLOR = GRAY
+# Настройки окна
+WIDTH = 800
+HEIGHT = 600
+FPS = 60
+
+# Игровые константы
+BALL_SPEED = 2
+ROCK_SPEED = 4
+SPAWN_TIMER = 60
 
 # Состояния игры
 MENU = "menu"
